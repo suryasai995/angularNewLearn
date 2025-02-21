d2a2132d050bd6464993c3c8f3a59bd0fd06dbd0
surya <suryad@yupptv.com>
2025-02-21 17:35:42 +0530

    domtv :- menu for mobile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),importProvidersFrom(HttpClientModule)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withInterceptors([
+        (req, next) => {
+          console.log('Intercepted Request:', req);
+          return next(req);
+        }
+      ])
+    )]
 };
--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { HttpclientService } from '../../services/httpclient.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'main-view',
+  imports:[CommonModule],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
 export class MainViewComponent {
+  userData:any=[]
 constructor(private _http:HttpclientService){
 
 }
 ngOnInit(): void {
   this._http.getUserData().subscribe((res)=>{
-
+    this.userData =res;
     console.log(res,"<<<<<< client Data");
   })
 }
